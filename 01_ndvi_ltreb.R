fe0f9107cf7574e82580037558d9acf6bd180ecd
Jeff Atkins <jwatkins6@vcu.edu>
2019-09-06 12:58:18 -0400

    more

--- 01_ndvi_ltreb.R
+++ 01_ndvi_ltreb.R
@@ -12,14 +12,17 @@ df <- read.csv("./data/ltreb_ndvi.csv")
 #### 
 # need to add scaling stuff in here....obviously. 
 
+#adding age
+df$stand.age <- 2019 - df$est_year
+
 # make total lai
 head(df[,15:19])
 
 df$lai <- rowMeans(df[,15:19], na.rm=TRUE)
 
 # scale CANOPY N using LMA generic 54.1 g per m^2 from Abrams and Kubiske (og is 5.41 mg per cm^2)
-df$can.N <- (df$nitrogen * 54.1) / 100
-
+df$can.N <- ((df$nitrogen * 54.1) / 100 ) * df$lai
+ 
 # chl adjust by lai
 df$chl_b_est <- df$chl_b * df$lai
 
@@ -46,6 +49,24 @@ ggplot(df, aes(x = lai, y = NDVI_IndexAvg, color = Plot))+
   xlab("NDVI")+
   stat_function(fun = lai.fun)
 
+x11()
+ggplot(df, aes(x = stand.age, y = NDVI_IndexAvg, color = Plot))+
+  geom_point(size = 4)+
+  ylab("NDVI")+
+  xlab(" Stand Age")
+
+x11()
+ggplot(df, aes(x = stand.age, y = can.N, color = Plot))+
+  geom_point(size = 4)+
+  ylab("Canopy N")+
+  xlab(" Stand Age")
+
+x11()
+ggplot(df, aes(x = stand.age, y = chl_b_est, color = Plot))+
+  geom_point(size = 4)+
+  ylab("Chl B")+
+  xlab(" Stand Age")
+
 # let's remove residuals now
 df$ndvi.resid <- abs( ((df$lai * 0.2024) + 0.072727) - df$NDVI_IndexAvg)
 
@@ -54,9 +75,17 @@ x11()
 ggplot(df, aes(y = ndvi.resid, x = Plot))+
   geom_boxplot()
 
+#let's plot these
+x11()
+ggplot(df, aes(y = can.N, x = Plot))+
+  geom_boxplot()
+
 # test stats
-lm.ndvi <- lm(NDVI_IndexAvg ~ chl_b_est, data = df)
-summary(lm.NDVI)
+lm.chl <- lm(chl_b_est ~ NDVI_IndexAvg, data = df)
+summary(lm.chl)
+
+lm.can.N <- lm(can.N ~ NDVI_IndexAvg, data = df)
+summary(lm.can.N)
 
 
 
@@ -68,6 +97,13 @@ df %>%
   summarise(ndvi = mean(NDVI_IndexAvg), ndvi.sd = sd(NDVI_IndexAvg),
             chl_b = mean(chl_b_est, na.rm = TRUE), chl_b.sd = sd(chl_b_est, na.rm = TRUE),
             nitro = mean(can.N, na.rm = TRUE), nitro.sd = sd(can.N, na.rm = TRUE)) -> df.means
+
+
+lm.chl.means <- lm(chl_b ~ ndvi, data = df.means)
+summary(lm.chl.means)
+
+lm.can.N.means <- lm(nitro ~ ndvi, data = df.means)
+summary(lm.can.N.means)
 
 # plot chl_b
 x11()
@@ -86,8 +122,22 @@ ggplot(df, aes(y = can.N, x = NDVI_IndexAvg))+
   xlab("NDVI")
 
 x11()
+ggplot(df, aes(y = chl_b_est, x = NDVI_IndexAvg))+
+  geom_point(size = 4)+
+  #geom_pointrange(aes(ymin = chl_b - chl_b.sd, ymax = chl_b + chl_b.sd))+ 
+  ylab("Chl B (g m^-2)")+
+  xlab("NDVI")
+
+x11()
 ggplot(df.means, aes(y = nitro, x = ndvi))+
   geom_point(size = 4)+
   #geom_pointrange(aes(ymin = chl_b - chl_b.sd, ymax = chl_b + chl_b.sd))+ 
   ylab("Canopy N (g m^-2)")+
   xlab("NDVI")
+
+x11()
+ggplot(df.means, aes(y = nitro, x = chl_b))+
+  geom_point(size = 4)+
+  #geom_pointrange(aes(ymin = chl_b - chl_b.sd, ymax = chl_b + chl_b.sd))+ 
+  ylab("Canopy N (g m^-2)")+
+  xlab("Chl B")
